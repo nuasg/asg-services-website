36b458f76507fd4ac6c3ee38b9136211a758825b
Michael Wang <michaelwang0712@gmail.com>
2015-05-10 23:43:20 -0500

    Some minor changes

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 // server.js
 
 // set up ======================================================================
-var express 		= require('express');
-var app 		= express(); 			// create app w/ express
-var port 		= process.env.PORT || 5000; 	// set port
+var express 	= require('express');
+var app 		= express();						// create app w/ express
+var port 		= process.env.PORT || 5000; 		// set port
 
 // express modules
-var morgan 		= require('morgan');
-var bodyParser 		= require('body-parser');
-var methodOverride 	= require('method-override');
+var morgan 		= require('morgan');				// debug messages
+var bodyParser 	= require('body-parser');			
+var methodOverride = require('method-override');
 
 // express config
-app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public'));		// set the static files location (e.g. /public/img will be /img for users)
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride());
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-//app.set('view engine', 'ejs');
 app.set('views', __dirname + '/public/views');
 
 // routes ======================================================================
